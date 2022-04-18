748c4ebc76456b16dfde700ea0c70ece9e01b53a
Thuyhaile <haithuyle2020@gmail.com>
2022-04-18 22:59:18 +0200

    tilføj file

---
+++ midi_json-backend/src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+//HTTP logger
+app.use(morgan('combined'))
+
+app.engine('hbs', handlebars.engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () =>
+  console.log(`Example app listening on port http://localhost:${port}`)
+)
