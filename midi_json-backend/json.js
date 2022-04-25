ba98283e89afe6fb0cd44173607cb4fe9b151e2e
Thuyhaile <haithuyle2020@gmail.com>
2022-04-25 17:31:53 +0200

    retter json.js på backend

--- midi_json-backend/json.js
+++ midi_json-backend/json.js
@@ -1,70 +1,39 @@
 
-// Demo program about processing json in backend
-var express = require('express');
-var app = express();
-var fs = require("fs");
+const http = require('http')
+const { parse } = require('querystring')
 const port = 3000
 
-// create temporary file
-let tempfile = "";
-import('tempfile').then(a => {
-    tempfile = a.default;
-});
-
-let input = tempfile
-var user1 = {
-   input1 : {
-      "ticks": 1921,
-      "time": 12.806666666666667,
-      "value": 0.0322265625
-   }
-}
-
-var user2 = {
-   input2 : {
-      "ticks": 1937,
-      "time": 12.913333333333334,
-      "value": 0.0966796875
-   }
-}
-var user3 = {
-   input3 : {
-      "ticks": 2280,
-      "time": 15.200000000000001,
-      "value": 0.9998779296875
-   }
-}
-
-var user4 = {
-   input4 : {
-      "ticks": 2399,
-      "time": 15.993333333333334,
-      "value": 0.2579345703125
-   }
-}
-var user5 = {
-   input5 : {
-      "ticks": 2524,
-      "time": 16.826666666666668,
-      "value": 0.0322265625
-   }
-}
-var user6 = {
-   input6 : {
-      "ticks": 2718,
-      "time": 18.12,
-      "value": 0.4837646484375
-   }
-}
-
-app.get('/users', function (req, res) {
-   fs.readFile( "input", 'utf8', function (err, data) {
-      data = Object.assign(user1, user2, user3, user4, user5, user6);
-      console.log( data );
-      res.end( JSON.stringify(data));
-   });
+let saveJson= []
+const server = http.createServer((req, res) => {
+   if (req.method === 'POST') {
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk
+        });
+        req.on('end', () => {
+            let a = parse(body)
+            saveJson.push(a)
+            res.end(JSON.stringify(saveJson))
+        })
+    }
+    else {
+        res.end(`
+        <!doctype html>
+        <html>
+        <body>
+        <label><b>Check receiving and synchronzing json files in server</b></label><br>
+        <label>(Inputdata is only for testing)</label><br><br>
+            <form action="/" method="post">
+                <input type="text" name="input" /><br><br>
+                <input type="number" name="number" /><br><br>
+                <input type="file" name="file" /><br><br>
+                <button>Send Json</button>
+            </form>
+        </body>
+        </html>
+        `)
+    }
 })
-
-app.listen(port, () =>{
- console.log(`App listening at http://localhost:${port}`)
+server.listen(port, () =>{
+   console.log(`App listening at http://localhost:${port}`)
 })
