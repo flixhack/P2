2acd48cc0d88eedab6ca57d178e8feb8d87b9bfc
Thuyhaile <haithuyle2020@gmail.com>
2022-05-11 21:09:07 +0200

    tilføj UI

--- midi_json-backend/Socket.io/public/handleData.js
+++ midi_json-backend/Socket.io/public/handleData.js
@@ -1,114 +1,421 @@
+var socket = io("http://localhost:3000")
 
-    var socket = io("http://localhost:3000")
+var userName=''
+var roomChoose=''
 
-    // simulate json from user, only check
-    var json1 = {"input1":{
-         "duration": 0.8023255813953488,
-         "durationTicks": 69,
-         "midi": 97,
-         "name": "C#7",
-         "ticks": 0,
-         "time": 0,
-         "velocity": 0.5039370078740157
-       }}
-     var json2 = {"input2":{
-         "duration": 1.0465116279069768,
-         "durationTicks": 90,
-         "midi": 99,
-         "name": "D#7",
-         "ticks": 7,
-         "time": 0.08139534883720931,
-         "velocity": 0.5511811023622047
-       }}
-     var json3 = {"input3":{
-         "duration": 0.18604651162790695,
-         "durationTicks": 16,
-         "midi": 103,
-         "name": "G7",
-         "ticks": 15,
-         "time": 0.1744186046511628,
-         "velocity": 0.5196850393700787
-       }}
-     var json4 = {"input4":{
-         "duration": 0.1046511627906977,
-         "durationTicks": 9,
-         "midi": 103,
-         "name": "G7",
-         "ticks": 32,
-         "time": 0.37209302325581395,
-         "velocity": 0.33070866141732286
-       }}
-     var json5 = {"input5":{
-         "duration": 0.6627906976744186,
-         "durationTicks": 57,
-         "midi": 101,
-         "name": "F7",
-         "ticks": 38,
-         "time": 0.4418604651162791,
-         "velocity": 0.4566929133858268
-       }}
-     var json6 = {"input6":{
-         "duration": 0.2209302325581396,
-         "durationTicks": 19,
-         "midi": 103,
-         "name": "G7",
-         "ticks": 42,
-         "time": 0.4883720930232558,
-         "velocity": 0.2204724409448819
-       }}
-     var json7 = {"input7":{
-         "duration": 0.046511627906976716,
-         "durationTicks": 4,
-         "midi": 103,
-         "name": "G7",
-         "ticks": 65,
-         "time": 0.7558139534883721,
-         "velocity": 0.07086614173228346
-       }}
-     var json8 = {"input8":{
-         "duration": 0.2209302325581457,
-         "durationTicks": 13,
-         "midi": 103,
-         "name": "G7",
-         "ticks": 42,
-         "time": 0.4883720930232558,
-         "velocity": 0.2204724409448819
-       }}
-     
-// Onclick events (button submit)
-function selectJson() {
-    let selectValue = document.getElementById("select").value
-    switch(selectValue) {
-      case '1':  
-        socket.emit("send Json", json1);
-        break
-      case '2':  
-        socket.emit("send Json", json2);
-        break
-      case '3':  
-        socket.emit("send Json", json3);
-        break
-      case '4':  
-        socket.emit("send Json", json4);
-        break
-      case '5':  
-        socket.emit("send Json", json5);
-        break
-      case '6':  
-        socket.emit("send Json", json6);
-        break
-      case '7':  
-        socket.emit("send Json", json7);
-        break
-      case '8':  
-        socket.emit("send Json", json8);
-        break
-      }
+// Don't use in project
+// Delete all these variables in project
+var json1 = {"input1":{
+    "duration": 0.8023255813953488,
+    "durationTicks": 69,
+    "midi": 97,
+    "name": "C#7",
+    "ticks": 0,
+    "time": 0,
+    "velocity": 0.5039370078740157
+  }}
+var json2 = {"input2":{
+    "duration": 1.0465116279069768,
+    "durationTicks": 90,
+    "midi": 99,
+    "name": "D#7",
+    "ticks": 7,
+    "time": 0.08139534883720931,
+    "velocity": 0.5511811023622047
+  }}
+var json3 = {"input3":{
+    "duration": 0.18604651162790695,
+    "durationTicks": 16,
+    "midi": 103,
+    "name": "G7",
+    "ticks": 15,
+    "time": 0.1744186046511628,
+    "velocity": 0.5196850393700787
+  }}
+var json4 = {"input4":{
+    "duration": 0.1046511627906977,
+    "durationTicks": 9,
+    "midi": 103,
+    "name": "G7",
+    "ticks": 32,
+    "time": 0.37209302325581395,
+    "velocity": 0.33070866141732286
+  }}
+var json5 = {"input5":{
+    "duration": 0.6627906976744186,
+    "durationTicks": 57,
+    "midi": 101,
+    "name": "F7",
+    "ticks": 38,
+    "time": 0.4418604651162791,
+    "velocity": 0.4566929133858268
+  }}
+var json6 = {"input6":{
+    "duration": 0.2209302325581396,
+    "durationTicks": 19,
+    "midi": 103,
+    "name": "G7",
+    "ticks": 42,
+    "time": 0.4883720930232558,
+    "velocity": 0.2204724409448819
+  }}
+var json7 = {"input7":{
+    "duration": 0.046511627906976716,
+    "durationTicks": 4,
+    "midi": 103,
+    "name": "G7",
+    "ticks": 65,
+    "time": 0.7558139534883721,
+    "velocity": 0.07086614173228346
+  }}
+var json8 = {"input8":{
+    "duration": 0.2209302325581457,
+    "durationTicks": 13,
+    "midi": 103,
+    "name": "G7",
+    "ticks": 42,
+    "time": 0.4883720930232558,
+    "velocity": 0.2204724409448819
+  }}
+
+// Choose json, only check 
+// In project only use commando (socket.emit ("send Json", json1))
+// to send midi data to server
+// "send Json" adjust accordingly (fx. "send midi to server")
+// json1 is replaced midi data
+
+function selectJson(json) {
+switch(json) {
+ case '1':  
+   socket.emit("send Json", json1);
+   break
+ case '2':  
+   socket.emit("send Json", json2);
+   break
+ case '3':  
+   socket.emit("send Json", json3);
+   break
+ case '4':  
+   socket.emit("send Json", json4);
+   break
+ case '5':  
+   socket.emit("send Json", json5);
+   break
+ case '6':  
+   socket.emit("send Json", json6);
+   break
+ case '7':  
+   socket.emit("send Json", json7);
+   break
+ case '8':  
+   socket.emit("send Json", json8);
+   break
+ }
 }
 
-// listen from server and show data on screen to check
-socket.on("send Json", function(data) {
-    document.getElementById("messages").innerText =(JSON.stringify(data));
-    window.scrollTo(0, document.body.scrollHeight);
+// Alert choose room
+socket.on("alert choose room", () => {alert("Please choose room!")})
+
+// listen from server and show data on screen to check 
+// In project use this but don't must show
+socket.on("send Json to client", function(data) {
+// In project midi array received her (data)
+// Data is handled in this function
+console.log(data)
+  document.getElementById("messages").innerText =(JSON.stringify(data)); //(this line don't use in project)
 });
-     
+
+// Choose json in the list to check
+// Don't use this function in project (delete)
+function SelectBox(obj){
+  var options = obj.children;
+  var choosejson = '';
+  for (var i = 0; i < options.length; i++){
+      if (options[i].selected){
+          choosejson   += options[i].value
+console.log(choosejson)
+          selectJson(choosejson)
+      }
+  }
+}
+
+// Hide button play music and chat in room
+function hideChatRoom(t) {
+    document.getElementById("boxMessage1").style.display = t
+}
+hideChatRoom("none")
+
+ // Hide chat with all users
+function hideChatAll(t) {
+    document.getElementById("boxMessage").style.display = t
+}
+hideChatAll("none")
+
+// Client register and send data to server
+var buttonRegister = document.getElementById("btnRegister");
+buttonRegister.onclick = function(){
+    var textContent = document.getElementById("textName").value
+    userName=textContent
+    if(textContent=='') {
+        alert('Please input your name!')
+    } else {
+        // Send data to server
+        socket.emit('client send username', textContent)
+        document.getElementById("textName").value='' 
+    }
+}
+
+// Receive failed register from server
+socket.on("register failure", function(){
+    alert('User exists!!!')
+})
+
+
+// Receive success register from server
+socket.on("register success", function(data) {
+    document.getElementById("currentUser").innerText = data +"!"
+    document.getElementById("btnLogout").removeAttribute("disabled")
+    document.getElementById("btnChat").removeAttribute("disabled")
+    document.getElementById("nameRoom").removeAttribute("disabled")
+    document.getElementById("btnCreateRoom").removeAttribute("disabled")
+    document.getElementById("btnRegister").disabled="disabled"
+})
+
+// Client login and send data to server
+var buttonLogin = document.getElementById("btnLogin");
+buttonLogin.onclick = function(){
+    var textContent = document.getElementById("textName").value
+    userName=textContent
+    if(textContent=='') {
+        alert('Please input your name!')
+    } else {
+        // Send data to server
+        socket.emit('client send login', textContent) 
+        document.getElementById("textName").value=''
+    }
+}
+
+// Log out and send data to server
+var buttonLogout = document.getElementById("btnLogout");
+buttonLogout.onclick = function(){
+    // Send data to server
+    socket.emit('logout') 
+    window.location.reload()
+}
+
+// Receive list of users from server and show them in screen
+socket.on("server send list users", function(data1, data2) {
+    var boxUsers = document.getElementById("boxContent")
+    boxUsers.innerText =''
+    let numberOnline = data1.length
+    // Show list of users
+    document.getElementById("numberOnline").innerText = ': '+ numberOnline
+    data1.forEach(function(user) {
+        boxUsers.innerHTML +=`${user}\n`
+    })
+    // Show list of rooms
+    let listRoom = document.getElementById("listRoom")
+    listRoom.innerText =''
+    let numberRoom = data2.length
+    for (let i = 0; i<numberRoom; i++){
+        var opt = document.createElement('option')
+        opt.value = data2[i].nameRoom
+        opt.innerHTML = data2[i].nameRoom
+        listRoom.appendChild(opt)
+    }
+})
+
+// Receive updating list of users efter client logout
+socket.on("server update list users", function(data) {
+    var boxUsers = document.getElementById("boxContent")
+    boxUsers.innerText =''
+    let numberOnline = data.length
+    document.getElementById("numberOnline").innerText = ': '+ numberOnline
+    data.forEach(function(i) {
+    boxUsers.innerHTML +=`${i}\n`
+   })
+})
+
+// Create new room and send data to server
+var buttonCreateRoom = document.getElementById("btnCreateRoom");
+buttonCreateRoom.onclick = function(){
+var textContent = document.getElementById("nameRoom").value
+    roomChoose=textContent
+    if(textContent=='') {
+        alert('Please input Rooms name!')
+    } else {
+        // Send data to server
+        socket.emit('client send roomName', textContent) 
+        document.getElementById("nameRoom").value=''
+    }
+}
+
+// Receive creating room from server
+socket.on("server send rooms", function(data) {
+    let listRoom = document.getElementById("listRoom")
+    listRoom.innerText =''
+    let numberRoom = data.length
+    // Show them in screen
+    for (let i = 0; i<numberRoom; i++){
+        var opt = document.createElement('option')
+        opt.value = data[i].nameRoom
+        opt.innerHTML = data[i].nameRoom
+        listRoom.appendChild(opt)
+    }
+})
+
+// Receive send rooms to client from server
+socket.on("server send rooms to client", function(data) {
+  
+console.log(data)
+    let numberRoom = data.length
+    let roomIn = document.getElementById("currentRoom")
+    let numberUser=document.getElementById("roomNow")
+    let listRoom=document.getElementById("contentRoom")
+    numberUser.innerText=''
+    listRoom.innerText =''
+    document.getElementById("btnChatMusic").removeAttribute("disabled")
+    document.getElementById("logoutRoom").removeAttribute("disabled")
+    for (var i = 0; i<numberRoom; i++){
+console.log(data[i].nameRoom)     
+        if((data[i].nameUser).includes(userName)) {
+            roomIn.innerText=': ' + data[i].nameRoom
+            numberUser.innerText=': '+ '1'
+            listRoom.innerText +=  `${(data[i].nameUser)}\n`
+        }
+    }
+})
+
+// Receive room exists from server
+socket.on("room exists", function() {
+    alert('Room exists!')
+})
+
+// Choose room from the list and send it to server
+function validateSelectBox(obj){
+    var options = obj.children;
+    var chooseRoom = '';
+    for (var i = 0; i < options.length; i++){
+        if (options[i].selected){
+            chooseRoom   += options[i].value
+            document.getElementById("currentRoom").innerText =': ' + chooseRoom
+            roomChoose=chooseRoom
+            // Send data to server
+            socket.emit("client sent choose room", chooseRoom, userName)
+        }
+    }
+    document.getElementById("logoutRoom").removeAttribute("disabled")
+    document.getElementById("btnChatMusic").removeAttribute("disabled")
+}
+
+// Receive choosing room from server
+socket.on("server send choose room", function(data){
+    let usersRoom = (data.nameUser).length
+    let boxRoom = document.getElementById("contentRoom")
+    boxRoom.innerText =''
+     document.getElementById("roomNow").innerText = ': '+ usersRoom
+     for (var i = 0; i<usersRoom; i++){
+          boxRoom.innerHTML += `${(data.nameUser)[i]}\n`
+    }
+})
+
+// Log out room and send data to server
+var buttonLogoutRoom = document.getElementById("logoutRoom");
+buttonLogoutRoom.onclick = function(){
+    // Send data to server
+    socket.emit('logoutRoom', roomChoose) 
+    buttonLogoutRoom.disabled="disabled"
+    document.getElementById("roomNow").innerText=''
+    document.getElementById("contentRoom").innerText=''
+    document.getElementById("currentRoom").innerText=''
+    document.getElementById("listMessage").innerText=''
+    document.getElementById("currentStatus").innerText=''
+    document.getElementById("btnChatMusic").disabled="disabled"
+    hideChatRoom("none")
+}
+
+// Receive update room from server
+socket.on("server send update room", function(data) {
+    let usersRoom = (data).length
+    let boxRoom = document.getElementById("contentRoom")
+    boxRoom.innerText =''
+    document.getElementById("roomNow").innerText = ': '+ usersRoom
+     for (var i = 0; i<usersRoom; i++){
+        boxRoom.innerHTML += `${data[i]}\n`
+    }
+})
+
+// Button chat with all users
+var buttonChatAll = document.getElementById("btnChat");
+buttonChatAll.onclick = function(){
+    document.getElementById("currentStatus").innerText = " with all users!"
+    document.getElementById("message").removeAttribute("disabled")
+    document.getElementById("btnSend").removeAttribute("disabled")
+    document.getElementById("listMessage").innerText=''
+    hideChatAll('')  
+    hideChatRoom("none")   
+}
+ 
+// Send message chat with all to server
+var buttonSend = document.getElementById("btnSend");
+buttonSend.onclick = function(){
+    var message = document.getElementById("message").value    
+    if(message=='') {
+        alert('Please write message!')
+    } else { 
+        // Send message to server
+        socket.emit('client send message', message) 
+        document.getElementById("message").value =''
+    }
+}
+
+// Receive data chat with all from server
+socket.on('server send message', function(data) {
+    var messages = document.getElementById("listMessage")
+    messages.innerHTML +=`${data.un}: ${data.txt}\n`
+})
+
+// Button chat in room 
+var buttonChatPlay = document.getElementById("btnChatMusic");
+buttonChatPlay.onclick = function(){
+    document.getElementById("currentStatus").innerText = " in room!"
+    document.getElementById("message1").removeAttribute("disabled")
+    document.getElementById("btnSend1").removeAttribute("disabled")
+    document.getElementById("listMessage").innerText=''
+    hideChatAll('none') 
+    hideChatRoom("")   
+}
+
+// Send message chat in room to server
+var buttonSend = document.getElementById("btnSend1");
+buttonSend.onclick = function(){
+    var message = document.getElementById("message1").value    
+    if(message=='') {
+        alert('Please write message!')
+    } else { 
+        // Send message to server
+        socket.emit('send message in room', message, `${roomChoose}`) 
+        document.getElementById("message1").value =''
+    }
+}
+
+// Receive data chat in room from server
+socket.on('server send message in room', function(data) {
+    var messages = document.getElementById("listMessage")
+    messages.innerHTML +=`${data.un}: ${data.txt}\n`
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- midi_json-backend/Socket.io/socket.js
+++ midi_json-backend/Socket.io/socket.js
@@ -1,22 +1,146 @@
-var express = require('express');
+var express = require('express')
 var app = express()
 app.use(express.static("public"))
-var http = require('http').createServer(app);
+
+var http = require('http').createServer(app)
 var io = require('socket.io')(http);
 var port = 3000
 
 var arrayJason = []
+var arrayUser = []
+var arrayRoom = []
+var listRoom = []
+
+function RoomData(nameRoom, nameUser, admin) {
+	this.nameRoom = nameRoom
+	this.nameUser = nameUser
+	this.admin = admin
+}
+
 io.on("connection", function(socket) {     
 	console.log('a user connected,'+' id:' + socket.id)  // check connect
-	socket.on('send Json', function(data){  // listen client
-		arrayJason.push(data)               // save json to array (synchronous)
-		console.log(arrayJason)  // check json on server
-		setTimeout(() => {io.sockets.emit('send Json', arrayJason)}, 8000)
-		//setTimeout(() => {arrayJason=[]}, 12000)  // delete arrays elementes
-	});
-});
- 
-// Render to web (socket.html) to communicate
+
+	// Receive register from client and check data
+	socket.on('client send username', function(data) {
+		socket.Username=data
+		if(arrayUser.indexOf(data)>= 0) {
+			// Send data to client
+			socket.emit("register failure")
+		} else {
+			arrayUser.push(data)
+			// Send data to clients
+			socket.emit("register success", data)
+			io.sockets.emit("server send list users", arrayUser, arrayRoom)
+		}
+	})
+
+	// Receive login from client
+	socket.on('client send login', function(data) {
+		socket.Username=data
+		if(arrayUser.indexOf(data)>= 0) {
+			// Send data to clients
+			socket.emit("register success", data)
+			io.sockets.emit("server send list users", arrayUser, arrayRoom)
+		}
+	})
+	 // Receive logout 		
+	socket.on('logout', function() {
+		arrayUser.splice(arrayUser.indexOf(socket.Username), 1)
+		io.sockets.emit('server update list users', arrayUser)
+	})
+	
+	// Receive creating new room from client 
+	socket.on('client send roomName', function(data){
+		socket.join(data)
+		socket.room=data
+		if(listRoom.length==0) {
+			listRoom.push(data)
+			arrayRoom.push(
+			new RoomData(socket.room, [socket.Username], socket.Username))
+			// Send data to client
+			socket.emit("server send rooms to client", arrayRoom)
+			io.sockets.emit("server send rooms",arrayRoom)
+		}else {
+			if (listRoom.indexOf(data)>= 0){
+				socket.emit("room exists")
+			} else {
+				listRoom.push(data)
+				arrayRoom.push(
+				new RoomData(socket.room, [socket.Username], socket.Username))
+				// Send data to client
+				socket.emit("server send rooms to client", arrayRoom)
+				io.sockets.emit("server send rooms",arrayRoom)
+			}
+		}
+	})
+
+	// Receive choose room from client
+	socket.on("client sent choose room", function(data1, data2) {
+		socket.join(data1)
+		socket.room=data1
+		for(let i=0; i<arrayRoom.length ; i++){
+			if(arrayRoom[i].nameRoom==data1) {
+				if(arrayRoom[i].nameUser.indexOf(data2)>= 0) {
+					io.sockets.in(socket.room).emit("server send choose room", arrayRoom[i])
+					break
+				} else {
+					arrayRoom[i].nameUser.push(data2)
+					io.sockets.in(socket.room).emit("server send choose room", arrayRoom[i])
+					break
+				}
+			}
+		}
+	})
+
+	// Receive logout room from client
+	socket.on('logoutRoom', function(data) {
+		arrayRoom.find(function(room) {
+			if(room.nameRoom==data) {
+				room.nameUser.splice((room.nameUser).indexOf(socket.Username), 1)
+				io.sockets.in(data).emit("server send update room", room.nameUser)
+			}
+		})
+		socket.leave(data)
+	})
+
+	// Receive message chat with all from client
+	socket.on('client send message', function(data) {
+	// Send data to client
+	io.sockets.emit('server send message', {un: socket.Username, txt: data})
+	})
+
+	// Receive message chat in room from client
+	socket.on('send message in room', function(data1, data2) {
+	// Send data to client
+	io.sockets.in(data2).emit('server send message in room', {un: socket.Username, txt: data1})
+	})	
+	
+	// Server receive midi data and synchronous them in array "arrayJason"
+	// Array midi "arrayJason" is sended to client 
+	socket.on('send Json', function(data){  
+		room = socket.room
+		if(room!=undefined) {
+			for(let i=0; i<arrayRoom.length ; i++){
+				if(arrayRoom[i].nameRoom==room) {
+					if(arrayRoom[i].nameUser.indexOf(socket.Username)>= 0) {
+						arrayJason.push(data) 
+						function sendData(r,arr) {
+							io.sockets.to(r).emit("send Json to client", arr);
+						}
+						setTimeout(sendData, 6000, room, arrayJason);
+						setTimeout(() => {arrayJason=[]}, 10000)
+						break
+					} else {
+						socket.emit("alert choose room")
+						break
+					}
+				}
+			}
+		} else {socket.emit("alert choose room")}
+	})
+})
+
+// Render to web (thu.html) to communicate with client
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/socket.html');
 });
